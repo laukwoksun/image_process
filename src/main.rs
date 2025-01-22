aed1537a61392988193bab04df998c6e1bb8c232
laukwoksun <xkuoliu@qq.com>
2025-01-23 02:24:47 +0800

    update project

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,33 @@ use walkdir::WalkDir;
 use image::{GenericImageView, ImageBuffer, Rgba};
 
 fn main() {
-    let folder_path = "E:\klzz\courses\L8_v1_d_01"; // 替换为实际的文件夹路径
-    process_folder(folder_path);
+    // 直接声明为 String 类型
+    let mut folder_path = r"E:\klzz\courses\L8_v1_d_01\Game1_LT1\laya\assets\game_zk\image".to_string(); 
+    folder_path = folder_path.replace("\\", "/");
+
+    let (total_png_count, processed_png_count) = process_folder(&folder_path);
+    println!("全部的 PNG 格式文件数量: {}", total_png_count);
+    println!("最终需要处理的 PNG 文件数量: {}", processed_png_count);
 }
 
-fn process_folder(folder_path: &str) {
+fn process_folder(folder_path: &str) -> (usize, usize) {
+    let mut total_png_count = 0;
+    let mut processed_png_count = 0;
+
     for entry in WalkDir::new(folder_path).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path();
         if path.extension().and_then(|s| s.to_str()) == Some("png") {
-            process_png(path);
+            total_png_count += 1;
+            if process_png(path) {
+                processed_png_count += 1;
+            }
         }
     }
+
+    (total_png_count, processed_png_count)
 }
 
-fn process_png(path: &Path) {
+fn process_png(path: &Path) -> bool {
     match image::open(path) {
         Ok(img) => {
             let (width, height) = img.dimensions();
@@ -27,12 +40,14 @@ fn process_png(path: &Path) {
                 if let Err(e) = std::fs::remove_file(path) {
                     eprintln!("Failed to delete file {}: {}", path.display(), e);
                 }
+                return true;
             }
         }
         Err(e) => {
             eprintln!("Failed to open image {}: {}", path.display(), e);
         }
     }
+    false
 }
 
 fn split_image(img: image::DynamicImage) -> Vec<ImageBuffer<Rgba<u8>, Vec<u8>>> {
